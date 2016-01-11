a9a46d6ba8ff4e2d95f3d08d0f5d6262e337f162
Tosh Afanasiev <tosh@tosh.me>
2016-01-11 21:33:47 +1100

    Slight refactor

--- cmd/pack/pack.go
+++ cmd/pack/pack.go
@@ -1,55 +1,66 @@
 package pack
 
 import (
-    "archive/tar"
-    "fmt"
-    "os"
-    "io/ioutil"
-    "io"
+	"archive/tar"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
 )
 
 const (
-    GO_LIB_EXT = ".a"
+	GO_LIB_EXT = ".a"
 )
 
 func Pack(pkg string, w io.Writer) error {
-    pkgdir := os.ExpandEnv("$GOPATH/pkg")
-    files, err := ioutil.ReadDir(pkgdir)
-    if err != nil {
-        return err
-    }
+	pkgdir := os.ExpandEnv("$GOPATH/pkg")
+	files, err := ioutil.ReadDir(pkgdir)
+	if err != nil {
+		return err
+	}
 
-    writer := tar.NewWriter(w)
-    defer func() {
-        if err := writer.Close(); err != nil {
-            panic(err)
-        }
-    }()
+	writer := tar.NewWriter(w)
+	defer func() {
+		if err := writer.Close(); err != nil {
+			panic(err)
+		}
+	}()
 
-    for _,f := range files {
-        if f.IsDir() {
-            // these should be of the form $GOOS_$GOARCH
-            name := fmt.Sprintf("%s/%s/%s%s", "pkg", f.Name(), pkg, GO_LIB_EXT)
-            file, err := os.Open(os.ExpandEnv("$GOPATH/") + name)
-            if err == nil {
-                defer file.Close()
+	for _, f := range files {
+		if f.IsDir() {
+			// these should be of the form $GOOS_$GOARCH
+			name := fmt.Sprintf("%s/%s/%s%s", "pkg", f.Name(), pkg, GO_LIB_EXT)
+			if err := pack_lib(writer, name); err != nil {
+				panic(err)
+			}
+		}
+	}
 
-                body, err := ioutil.ReadAll(file)
-                if err != nil {
-                    panic(err)
-                }
+	return nil
+}
 
-                header := tar.Header{
-                    Name: name,
-                    Mode: 0444,
-                    Size: int64(len(body)),
-                }
+func pack_lib(writer *tar.Writer, name string) error {
+	fname := os.ExpandEnv("$GOPATH/") + name
+	file, err := os.Open(fname)
+	if err != nil {
+		return err
+	}
 
-                writer.WriteHeader(&header)
-                writer.Write(body)
-            }
-        }
-    }
+	defer file.Close()
 
-    return nil
+	body, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	header := tar.Header{
+		Name: name,
+		Mode: 0444,
+		Size: int64(len(body)),
+	}
+
+	writer.WriteHeader(&header)
+	writer.Write(body)
+
+	return nil
 }
